aaf1d3d1f074f0fec1cb5785dc5d4b2defe19f19
nukosuke <39.nuko@gmail.com>
2016-05-05 18:54:11 +0900

    add assets task

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,17 @@ var gulp    = require('gulp');
 var webpack = require('gulp-webpack');
 var shell   = require('gulp-shell');
 
-gulp.task('default', ['build']);
+gulp.task('default', ['build', 'assets']);
 
 gulp.task('build', function() {
   gulp.src(['src/js/main.js'])
   .pipe(webpack(require('./webpack.config')))
-  .pipe(gulp.dest('build'));
+  .pipe(gulp.dest('build/js'));
 });
 
-gulp.task('run', ['build'], shell.task('npm start'));
+gulp.task('assets', function() {
+  gulp.src(['src/img/*'])
+  .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('run', ['default'], shell.task('npm start'));
